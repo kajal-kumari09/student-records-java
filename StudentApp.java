929e67a3b42627c419dc2a2b4835a5ea5ab4bf51
kajal-kumari09 <kajalkumari091803@gmail.com>
2026-04-07 11:49:28 +0530

    Create StudentApp.java

---
+++ StudentApp.java
@@ -0,0 +1,9 @@
+public class StudentApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Student Management System");
+
+        // Start main system
+        StudentManagement.main(args);
+    }
+}
